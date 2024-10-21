32b1282d9eb2da2a34f9d65629bf9aa19233609f
Vui-Chee <vuicheesiew@gmail.com>
2024-10-21 17:39:07 +0800

    Swap pos for type in log

--- src/main.rs
+++ src/main.rs
@@ -190,7 +190,7 @@ async fn main() -> anyhow::Result<()> {
                 }
 
                 tracing::info!(
-                    "{}. type = {:?}, {}, state = {:?}, {:?}",
+                    "{}. {:?}, type = {}, state = {:?}, {:?}",
                     i + 1,
                     name,
                     instance.instance_type.as_ref().unwrap(),
